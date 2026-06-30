f9ce74482f6754fd63c0e61310cbd817dcbe5d2f
Dylan <hello@erendirasboutique.com>
2026-06-29 22:00:34 -0700

    Remove session check from login page

--- app/login/page.js
+++ app/login/page.js
@@ -1,27 +1,9 @@
 'use client';
 
-import { useEffect, useState } from 'react';
 import { supabaseClient } from '@/lib/supabaseClient';
 import { FcGoogle } from 'react-icons/fc';
 
 export default function LoginPage() {
-  const [checking, setChecking] = useState(true);
-
-  useEffect(() => {
-    async function checkSession() {
-      const { data } = await supabaseClient.auth.getSession();
-
-      if (data?.session) {
-        window.location.replace('/');
-        return;
-      }
-
-      setChecking(false);
-    }
-
-    checkSession();
-  }, []);
-
   async function loginWithGoogle() {
     await supabaseClient.auth.signInWithOAuth({
       provider: 'google',
@@ -29,16 +11,6 @@ export default function LoginPage() {
         redirectTo: 'https://ship.erendirasboutique.com/login'
       }
     });
-  }
-
-  if (checking) {
-    return (
-      <main className="login-wrap">
-        <section className="login-card glass">
-          <p>Checking login...</p>
-        </section>
-      </main>
-    );
   }
 
   return (
