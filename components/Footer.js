66c423ce08f9071f82512598e0da885d5abfe04f
Dylan <hello@erendirasboutique.com>
2026-06-30 01:51:51 -0700

    Add Footer component for shipping portal

---
+++ components/Footer.js
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="green-divider" />
+      <p>Erendira&apos;s Boutique · Shipping Portal</p>
+    </footer>
+  );
+}
